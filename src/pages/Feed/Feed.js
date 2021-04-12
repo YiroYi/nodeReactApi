b8f7d2d6da364e3d679f4baabc334c6ea4c14fea
YiroYi <yiroyi@live.com>
2021-04-11 20:00:39 -0500

    Add: Editing functionality one item

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -59,7 +59,12 @@ class Feed extends Component {
       })
       .then(resData => {
         this.setState({
-          posts: resData.posts,
+          posts: resData.posts.map(post => {
+            return {
+              ...post,
+              imagePath: post.imageUrl
+            }
+          }),
           totalPosts: resData.totalItems,
           postsLoading: false
         });
@@ -116,7 +121,8 @@ class Feed extends Component {
     let method = 'POST'
 
     if (this.state.editPost) {
-      url = 'URL';
+      url = 'http://127.0.0.1:3000/feed/post/' + this.state.editPost._id;
+      method = 'PUT';
     }
 
     fetch(url, {
